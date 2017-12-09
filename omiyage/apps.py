a9749229073e3bd8cc23fcce2e86962be1223203
Nao Takegawa <nao.takegawa316@gmail.com>
2017-12-09 14:42:08 +0900

    initial commit

---
+++ omiyage/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OmiyageConfig(AppConfig):
+    name = 'omiyage'
---
+++ omiyage/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model)
+    user_id = models.CharField
+    password = models.CharField
+    gender = models.IntegerField
+    age = models.IntegerField
+    notification_flg = models.BooleanField(default=false)
+    name = models.CharField
+    born_in = models.IntegerField
+    email = models.EmailField
+    home_address = models.CharField
+    phone_number = models.IntegerField
+    pass
+
+class Product(models.Model)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Review(models.Model)
+
+class Area(models.Model)
+    name = models.CharField
+
+class Genre(models.Model)
+    name = models.CharField
+
+class Maker(models.Model)
+    name = models.CharField
+
+class ProductPictures(models.Model)
