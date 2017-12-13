f1a9353bdddf90f0647b83913db72b3ad448c63f
Nao Takegawa <nao.takegawa316@gmail.com>
2017-12-13 21:49:57 +0900

    preserve changes

--- omiyage/views.py
+++ omiyage/views.py
@@ -4,6 +4,5 @@ from django.template import loader
 
 # Create your views here.
 def index(request):
-    header = loader.get_template('omiyage/layout/header.html')
     template = loader.get_template('omiyage/index.html')
     return HttpResponse(template.render())
